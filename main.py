900b9f143d0d693112074a73d7f8e5d412c0689e
AlTosterino <altosterino@gmail.com>
2020-01-04 21:49:36 +0100

    Added Light class, changed Lights to LightsDriver

--- main.py
+++ main.py
@@ -4,26 +4,15 @@ init()
 class TreeError(Exception):
     pass
 
-class Lights():
-    # Create separate class for this
-    colors = {
-        'R': {
-            'color': Fore.RED,
-            'repl': '●'
-        },
-        'G': {
-            'color': Fore.GREEN,
-            'repl': '●'
-        },
-        'B': {
-            'color': Fore.BLUE,
-            'repl': '●'
-        },
-        'Y': {
-            'color': Fore.YELLOW,
-            'repl': '*'
-        } 
-    }
+class Light():
+
+    def __init__(self, replace_char: str, color_char: str, color: Fore):
+        self.replace_char = replace_char
+        self.color = color
+        self.color_char = color_char
+
+class LightsDriver():
+    colors = {}
     reset = Style.RESET_ALL
 
     def color(self, char: str) -> str:
@@ -34,10 +23,13 @@ class Lights():
 
     def color_generator(self, chars: list) -> str:
         for char in chars:
-            if clr := self.colors.get(char):
-                yield f'{clr["color"]}{clr["repl"]}'
+            if light := self.colors.get(char):
+                yield f'{light.color}{light.replace_char}'
             else:
                 yield f'{self.reset}{char}'
+
+    def add_light(self, light_obj: Light):
+        self.colors.setdefault(light_obj.color_char, light_obj)
 
 
 class Tree():
@@ -59,7 +51,11 @@ class Tree():
 
 if __name__ == '__main__':
     tree_obj = Tree('christmas_tree.txt')
-    lights_obj = Lights()
+    lights_obj = LightsDriver()
     chars = tree_obj.open_file()
+    wanted_colors = (('R', '●', Fore.RED), ('G', '●', Fore.GREEN), ('B', '●', Fore.BLUE), ('Y', '*', Fore.YELLOW))
+    for item in wanted_colors:
+        color_char, replace_char, color = item
+        lights_obj.add_light(Light(replace_char, color_char, color))
     print(''.join(lights_obj.color_generator(chars)))
     
